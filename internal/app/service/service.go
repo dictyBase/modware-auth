647d50c51c364a8dd3ace2511e6d71a048460690
wildlifehexagon <crossthebreeze@gmail.com>
2020-01-16 17:13:25 -0600

    feat: fill out grpc service stubs

--- internal/app/service/service.go
+++ internal/app/service/service.go
@@ -1,1 +1,60 @@
 package service
+
+import (
+	"context"
+	"github.com/dictyBase/aphgrpc"
+	"github.com/dictyBase/go-genproto/dictybaseapis/auth"
+	"github.com/dictyBase/modware-auth/internal/message"
+	"github.com/dictyBase/modware-auth/internal/repository"
+	"github.com/golang/protobuf/ptypes/empty"
+)
+
+// AuthService is the container for managing auth service definitions
+type AuthService struct {
+	*aphgrpc.Service
+	repo      repository.AuthRepository
+	publisher message.Publisher
+}
+
+func defaultOptions() *aphgrpc.ServiceOptions {
+	return &aphgrpc.ServiceOptions{Resource: "auth"}
+}
+
+// NewAuthService is the constructor for creating a new instance of AuthService
+func NewAuthService(repo repository.AuthRepository, pub message.Publisher, opt ...aphgrpc.Option) *AuthService {
+	so := defaultOptions()
+	for _, optfn := range opt {
+		optfn(so)
+	}
+	srv := &aphgrpc.Service{}
+	aphgrpc.AssignFieldsToStructs(so, srv)
+	return &AuthService{
+		Service:   srv,
+		repo:      repo,
+		publisher: pub,
+	}
+}
+
+func (s *AuthService) Login(ctx context.Context, l *auth.NewLogin) (*auth.Auth, error) {
+	a := &auth.Auth{}
+
+	return a, nil
+}
+
+func (s *AuthService) Relogin(ctx context.Context, l *auth.NewRelogin) (*auth.Auth, error) {
+	a := &auth.Auth{}
+
+	return a, nil
+}
+
+func (s *AuthService) GetRefreshToken(ctx context.Context, t *auth.NewToken) (*auth.Token, error) {
+	tkn := &auth.Token{}
+
+	return tkn, nil
+}
+
+func (s *AuthService) Logout(ctx context.Context, t *auth.NewRefreshToken) (*empty.Empty, error) {
+	e := &empty.Empty{}
+
+	return e, nil
+}
