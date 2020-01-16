451f83a88086f7ab610e1e79198d45ae002714e3
wildlifehexagon <crossthebreeze@gmail.com>
2020-01-16 16:42:38 -0600

    feat: add interface to publish message for auth microservice

--- internal/message/message.go
+++ internal/message/message.go
@@ -1,1 +1,13 @@
 package message
+
+import (
+	"github.com/dictyBase/go-genproto/dictybaseapis/auth"
+)
+
+// Publisher manages publishing of message
+type Publisher interface {
+	// Publis publishes the auth object using the given subject
+	Publish(subject string, ann *auth.Auth) error
+	// Close closes the connection to the underlying messaging server
+	Close() error
+}
