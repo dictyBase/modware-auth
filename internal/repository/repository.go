dbbbc1d152527a84909a1fa8c5ca995d278002e5
wildlifehexagon <crossthebreeze@gmail.com>
2020-01-16 12:11:45 -0600

    refactor: implement redis as repository, not storage

---
+++ internal/repository/repository.go
@@ -0,0 +1,10 @@
+package repository
+
+import "time"
+
+type AuthRepository interface {
+	GetToken(string) (string, error)
+	SetToken(string, string, time.Duration) error
+	DeleteToken(...string) error
+	HasToken(string) (bool, error)
+}
--- internal/storage/storage.go
+++
@@ -1,9 +0,0 @@
-package storage
-
-type Storage interface {
-	Get(string, string) (string, error)
-	Set(string, string, string) error
-	Delete(string, ...string) error
-	IsExist(string, string) bool
-	Close() error
-}
