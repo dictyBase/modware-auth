3ce97c94a58dc4098b84448b865d5fc83ab94e34
Siddhartha Basu <biosidd@gmail.com>
2020-01-18 10:59:45 -0600

    feat: add separate file for error management code

---
+++ internal/jwtauth/error.go
@@ -0,0 +1,44 @@
+package jwtauth
+
+import (
+	"errors"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+var (
+	ErrUnauthorized = errors.New("jwtauth: token is unauthorized")
+	ErrExpired      = errors.New("jwtauth: token is expired")
+	ErrNBFInvalid   = errors.New("jwtauth: token nbf validation failed")
+	ErrIATInvalid   = errors.New("jwtauth: token iat validation failed")
+	ErrNoTokenFound = errors.New("jwtauth: no token found")
+	ErrAlgoInvalid  = errors.New("jwtauth: algorithm mismatch")
+)
+
+func isValidationNotValidYet(err *jwt.ValidationError) bool {
+	if err.Errors == jwt.ValidationErrorNotValidYet {
+		return true
+	}
+	return false
+}
+
+func isValidationIssuedAt(err *jwt.ValidationError) bool {
+	if err.Errors == jwt.ValidationErrorIssuedAt {
+		return true
+	}
+	return false
+}
+
+func isValidationExpired(err *jwt.ValidationError) bool {
+	if err.Errors == jwt.ValidationErrorExpired {
+		return true
+	}
+	return false
+}
+
+func isValidationError(err error) bool {
+	if _, ok := err.(*jwt.ValidationError); ok {
+		return true
+	}
+	return false
+}
