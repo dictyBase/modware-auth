6fddefebc6c2bd898b92f32e0dd7641a26333077
wildlifehexagon <crossthebreeze@gmail.com>
2020-01-21 10:22:09 -0600

    refactor: check for nonexistent token in redis delete method

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -5,6 +5,6 @@ import "time"
 type AuthRepository interface {
 	GetToken(string) (string, error)
 	SetToken(string, string, time.Duration) error
-	DeleteToken(...string) error
+	DeleteToken(string) error
 	HasToken(string) (bool, error)
 }
