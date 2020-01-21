ef9fbe7fc56a1dcfa30b07e80d03b01b1bb09d87
wildlifehexagon <crossthebreeze@gmail.com>
2020-01-21 11:57:32 -0600

    test: add unit tests for jwt Verify method

--- internal/jwtauth/jwtauth_test.go
+++ internal/jwtauth/jwtauth_test.go
@@ -46,5 +46,72 @@ func TestEncode(t *testing.T) {
 	}
 	_, err = ja.Encode(claims)
 	assert := assert.New(t)
-	assert.NoError(err, "expect to no error for jwt encoding")
+	assert.NoError(err, "expect no error for jwt encoding")
+}
+
+func TestVerify(t *testing.T) {
+	assert := assert.New(t)
+	private, public, err := generateKeys()
+	if err != nil {
+		t.Error(err)
+	}
+	ja := NewJwtAuth(jwt.SigningMethodRS512, private, public)
+	claims := jwt.StandardClaims{
+		Issuer:    "dictyBase",
+		Subject:   "dictyBase login token",
+		ExpiresAt: time.Now().Add(time.Hour * 240).Unix(),
+		IssuedAt:  time.Now().Unix(),
+		NotBefore: time.Now().Unix(),
+		Id:        xid.New().String(),
+		Audience:  "user",
+	}
+	val, err := ja.Encode(claims)
+	assert.NoError(err, "expect no error for jwt encoding")
+	_, err = ja.Verify(val)
+	assert.NoError(err, "expect no error when verifying valid jwt")
+
+	claimsBadExpiresAt := jwt.StandardClaims{
+		Issuer:    "dictyBase",
+		Subject:   "dictyBase login token",
+		ExpiresAt: time.Now().Unix() - 1000000, // make expired
+		IssuedAt:  time.Now().Unix(),
+		NotBefore: time.Now().Unix(),
+		Id:        xid.New().String(),
+		Audience:  "user",
+	}
+	val2, err := ja.Encode(claimsBadExpiresAt)
+	assert.NoError(err, "expect no error for jwt encoding")
+	_, err = ja.Verify(val2)
+	assert.Error(err, "expect error with expired jwt token")
+
+	claimsBadIssuedAt := jwt.StandardClaims{
+		Issuer:    "dictyBase",
+		Subject:   "dictyBase login token",
+		ExpiresAt: time.Now().Add(time.Hour * 240).Unix(),
+		IssuedAt:  time.Now().Unix() + 100000, // set issued time in future
+		NotBefore: time.Now().Unix(),
+		Id:        xid.New().String(),
+		Audience:  "user",
+	}
+	val3, err := ja.Encode(claimsBadIssuedAt)
+	assert.NoError(err, "expect no error for jwt encoding")
+	_, err = ja.Verify(val3)
+	assert.Error(err, "expect error with bad issued at time")
+
+	claimsBadNotBefore := jwt.StandardClaims{
+		Issuer:    "dictyBase",
+		Subject:   "dictyBase login token",
+		ExpiresAt: time.Now().Add(time.Hour * 240).Unix(),
+		IssuedAt:  time.Now().Unix(),
+		NotBefore: time.Now().Unix() + 100000, // make token not valid yet
+		Id:        xid.New().String(),
+		Audience:  "user",
+	}
+	val4, err := ja.Encode(claimsBadNotBefore)
+	assert.NoError(err, "expect no error for jwt encoding")
+	_, err = ja.Verify(val4)
+	assert.Error(err, "expect error with not valid yet token")
+
+	_, err = ja.Verify("")
+	assert.Error(err, "expect error with empty token string")
 }
