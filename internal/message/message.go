080026cbe26293d71c277f41db9e97c24e8f63b3
wildlifehexagon <crossthebreeze@gmail.com>
2020-01-23 10:26:13 -0600

    feat: add nats request interface and methods

--- internal/message/message.go
+++ internal/message/message.go
@@ -1,7 +1,11 @@
 package message
 
 import (
+	"context"
+	"time"
+
 	"github.com/dictyBase/go-genproto/dictybaseapis/auth"
+	"github.com/dictyBase/go-genproto/dictybaseapis/pubsub"
 )
 
 // Publisher manages the publishing of messages
@@ -10,4 +14,13 @@ type Publisher interface {
 	PublishTokens(subject string, token *auth.Token) error
 	// Close closes the connection to the underlying messaging server
 	Close() error
+}
+
+// Request handles requesting any messages
+type Request interface {
+	IsActive() bool
+	UserRequest(string, *pubsub.IdRequest, time.Duration) (*pubsub.UserReply, error)
+	UserRequestWithContext(context.Context, string, *pubsub.IdRequest) (*pubsub.UserReply, error)
+	IdentityRequest(string, *pubsub.IdentityReq, time.Duration) (*pubsub.IdentityReply, error)
+	IdentityRequestWithContext(context.Context, string, *pubsub.IdentityReq) (*pubsub.IdentityReply, error)
 }
