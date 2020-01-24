da9340885bf1be19e3b896c1790c7341aa2a0842
wildlifehexagon <crossthebreeze@gmail.com>
2020-01-24 14:48:13 -0600

    feat: add flag validation

---
+++ internal/app/validate/validate.go
@@ -0,0 +1,29 @@
+package validate
+
+import (
+	"fmt"
+
+	"github.com/urfave/cli"
+)
+
+// ServerArgs validates that the necessary flags are not missing
+func ServerArgs(c *cli.Context) error {
+	for _, p := range []string{
+		"user-grpc-host",
+		"user-grpc-port",
+		"identity-grpc-host",
+		"identity-grpc-port",
+		"nats-host",
+		"nats-port",
+		"redis-master-service-host",
+		"redis-master-service-port",
+	} {
+		if len(c.String(p)) == 0 {
+			return cli.NewExitError(
+				fmt.Sprintf("argument %s is missing", p),
+				2,
+			)
+		}
+	}
+	return nil
+}
