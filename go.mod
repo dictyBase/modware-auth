2dd93a4e1b2388097aaa4f576b48b83874308480
wildlifehexagon <crossthebreeze@gmail.com>
2020-01-24 16:06:30 -0600

    feat: implement RunServer command

--- go.mod
+++ go.mod
@@ -3,15 +3,16 @@ module github.com/dictyBase/modware-auth
 require (
 	cloud.google.com/go v0.52.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/dictyBase/aphgrpc v1.3.0
-	github.com/dictyBase/go-genproto v0.0.0-20200115211547-651977b0b4d6
-	github.com/go-playground/validator/v10 v10.0.1
+	github.com/dictyBase/go-genproto v0.0.0-20200122001226-b1ccbdee4aa3
+	github.com/go-playground/validator/v10 v10.1.0
 	github.com/go-redis/redis/v7 v7.0.0-beta.5
 	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/golang/protobuf v1.3.2
+	github.com/grpc-ecosystem/go-grpc-middleware v1.1.0
 	github.com/grpc-ecosystem/grpc-gateway v1.12.1 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/kr/pretty v0.2.0 // indirect
 	github.com/mwitkow/go-proto-validators v0.3.0 // indirect
 	github.com/nats-io/gnatsd v1.4.1 // indirect
@@ -21,11 +22,13 @@ require (
 	github.com/onsi/ginkgo v1.11.0 // indirect
 	github.com/onsi/gomega v1.8.1 // indirect
 	github.com/rs/xid v1.2.1
+	github.com/sirupsen/logrus v1.4.2
 	github.com/stretchr/testify v1.4.0
 	github.com/urfave/cli v1.22.2
 	golang.org/x/crypto v0.0.0-20200117160349-530e935923ad // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 	golang.org/x/sys v0.0.0-20200122134326-e047566fdf82 // indirect
 	google.golang.org/genproto v0.0.0-20200122232147-0452cf42e150 // indirect
+	google.golang.org/grpc v1.26.0
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -1,1 +1,179 @@
 package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	"github.com/dictyBase/modware-auth/internal/jwtauth"
+
+	"github.com/dictyBase/aphgrpc"
+	"github.com/dictyBase/go-genproto/dictybaseapis/auth"
+	"github.com/dictyBase/go-genproto/dictybaseapis/identity"
+	"github.com/dictyBase/go-genproto/dictybaseapis/user"
+	"github.com/dictyBase/modware-auth/internal/app/service"
+	"github.com/dictyBase/modware-auth/internal/message/nats"
+	"github.com/dictyBase/modware-auth/internal/oauth"
+	"github.com/dictyBase/modware-auth/internal/repository/redis"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_logrus "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
+	gnats "github.com/nats-io/go-nats"
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+func RunServer(c *cli.Context) error {
+	rrepo, err := redis.NewAuthRepo(fmt.Sprintf("%s:%s", c.String("redis-master-service-host"), c.String("redis-master-service-port")))
+	if err != nil {
+		return cli.NewExitError(
+			fmt.Sprintf("cannot connect to redis auth repository %s", err.Error()),
+			2,
+		)
+	}
+	ms, err := nats.NewPublisher(
+		c.String("nats-host"), c.String("nats-port"),
+		gnats.MaxReconnects(-1), gnats.ReconnectWait(2*time.Second),
+	)
+	if err != nil {
+		return cli.NewExitError(
+			fmt.Sprintf("cannot connect to messaging server %s", err.Error()),
+			2,
+		)
+	}
+	// establish grpc connections
+	uconn, err := grpc.Dial(fmt.Sprintf("%s:%s", c.String("user-grpc-host"), c.String("user-grpc-port")), grpc.WithInsecure())
+	if err != nil {
+		return cli.NewExitError(
+			fmt.Sprintf("cannot connect to grpc user microservice %s", err),
+			2,
+		)
+	}
+	iconn, err := grpc.Dial(fmt.Sprintf("%s:%s", c.String("identity-grpc-host"), c.String("identity-grpc-port")), grpc.WithInsecure())
+	if err != nil {
+		return cli.NewExitError(
+			fmt.Sprintf("cannot connect to grpc identity microservice %s", err),
+			2,
+		)
+	}
+	config, err := readSecretConfig(c)
+	if err != nil {
+		return cli.NewExitError(fmt.Sprintf("Unable to secret config file %q\n", err), 2)
+	}
+	jt, err := parseJwtKeys(c)
+	if err != nil {
+		return cli.NewExitError(fmt.Sprintf("Unable to parse keys %q\n", err), 2)
+	}
+	grpcS := grpc.NewServer(
+		grpc_middleware.WithUnaryServerChain(
+			grpc_ctxtags.UnaryServerInterceptor(),
+			grpc_logrus.UnaryServerInterceptor(getLogger(c)),
+		),
+	)
+	srv, err := service.NewAuthService(&service.ServiceParams{
+		Repository:      rrepo,
+		Publisher:       ms,
+		User:            user.NewUserServiceClient(uconn),
+		Identity:        identity.NewIdentityServiceClient(iconn),
+		JWTAuth:         *jt,
+		ProviderSecrets: *config,
+		Options: []aphgrpc.Option{
+			aphgrpc.TopicsOption(map[string]string{
+				"tokenCreate": "AuthService.Create",
+			}),
+		},
+	})
+	auth.RegisterAuthServiceServer(grpcS, srv)
+	reflection.Register(grpcS)
+	// create listener
+	endP := fmt.Sprintf(":%s", c.String("port"))
+	lis, err := net.Listen("tcp", endP)
+	if err != nil {
+		return cli.NewExitError(
+			fmt.Sprintf("failed to listen %s", err), 2,
+		)
+	}
+	log.Printf("starting grpc server on %s", endP)
+	if err := grpcS.Serve(lis); err != nil {
+		return cli.NewExitError(err.Error(), 2)
+	}
+	return nil
+}
+
+// Reads the configuration file containing the various client secret keys
+// of the providers. The expected format will be ...
+//  {
+//		"google": "xxxxxxxxxxxx",
+//		"orcid": "xxxxxxxx",
+//	}
+func readSecretConfig(c *cli.Context) (*oauth.ProviderSecrets, error) {
+	var provider *oauth.ProviderSecrets
+	reader, err := os.Open(c.String("config"))
+	defer reader.Close()
+	if err != nil {
+		return provider, err
+	}
+	if err := json.NewDecoder(reader).Decode(&provider); err != nil {
+		return provider, err
+	}
+	return provider, nil
+}
+
+// Reads the public and private keys from their respective files and
+// stores them in the jwt handler.
+func parseJwtKeys(c *cli.Context) (*jwtauth.JWTAuth, error) {
+	jh := &jwtauth.JWTAuth{}
+	private, err := ioutil.ReadFile(c.String("private-key"))
+	if err != nil {
+		return jh, err
+	}
+	pkey, err := jwt.ParseRSAPrivateKeyFromPEM(private)
+	if err != nil {
+		return jh, err
+	}
+	public, err := ioutil.ReadFile(c.String("public-key"))
+	if err != nil {
+		return jh, err
+	}
+	pubkey, err := jwt.ParseRSAPublicKeyFromPEM(public)
+	if err != nil {
+		return jh, err
+	}
+	return jwtauth.NewJwtAuth(jwt.SigningMethodRS512, pkey, pubkey), err
+}
+
+func getLogger(c *cli.Context) *logrus.Entry {
+	log := logrus.New()
+	log.Out = os.Stderr
+	switch c.GlobalString("log-format") {
+	case "text":
+		log.Formatter = &logrus.TextFormatter{
+			TimestampFormat: "02/Jan/2006:15:04:05",
+		}
+	case "json":
+		log.Formatter = &logrus.JSONFormatter{
+			TimestampFormat: "02/Jan/2006:15:04:05",
+		}
+	}
+	l := c.GlobalString("log-level")
+	switch l {
+	case "debug":
+		log.Level = logrus.DebugLevel
+	case "warn":
+		log.Level = logrus.WarnLevel
+	case "error":
+		log.Level = logrus.ErrorLevel
+	case "fatal":
+		log.Level = logrus.FatalLevel
+	case "panic":
+		log.Level = logrus.PanicLevel
+	}
+	return logrus.NewEntry(log)
+}
