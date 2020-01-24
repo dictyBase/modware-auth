ff77bd289042c9c2aae30e8eac38b639fd2bd602
wildlifehexagon <crossthebreeze@gmail.com>
2020-01-24 16:47:40 -0600

    refactor: separate getServerFlags into smaller functions

--- cmd/modware-auth/main.go
+++ cmd/modware-auth/main.go
@@ -59,56 +59,9 @@ func main() {
 
 func getServerFlags() []cli.Flag {
 	var f []cli.Flag
-	f = append(
-		f,
-		[]cli.Flag{
-			cli.StringFlag{
-				Name:   "config, c",
-				Usage:  "config file (required)",
-				EnvVar: "OAUTH_CONFIG",
-			},
-			cli.StringFlag{
-				Name:   "pkey, public-key",
-				Usage:  "public key file for verifying jwt",
-				EnvVar: "JWT_PUBLIC_KEY",
-			},
-			cli.StringFlag{
-				Name:   "private-key, prkey",
-				Usage:  "private key file for signing jwt",
-				EnvVar: "JWT_PRIVATE_KEY",
-			},
-			cli.StringFlag{
-				Name:   "user-grpc-host",
-				EnvVar: "USER_API_SERVICE_HOST",
-				Usage:  "user grpc host",
-			},
-			cli.StringFlag{
-				Name:   "user-grpc-port",
-				EnvVar: "USER_API_SERVICE_PORT",
-				Usage:  "user grpc port",
-			},
-			cli.StringFlag{
-				Name:   "identity-grpc-host",
-				EnvVar: "IDENTITY_API_SERVICE_HOST",
-				Usage:  "identity grpc host",
-			},
-			cli.StringFlag{
-				Name:   "identity-grpc-port",
-				EnvVar: "IDENTITY_API_SERVICE_PORT",
-				Usage:  "identity grpc port",
-			},
-			cli.StringFlag{
-				Name:   "redis-master-service-host",
-				EnvVar: "REDIS_MASTER_SERVICE_HOST",
-				Usage:  "redis master grpc host",
-			},
-			cli.StringFlag{
-				Name:   "redis-master-service-port",
-				EnvVar: "REDIS_MASTER_SERVICE_PORT",
-				Usage:  "redis master grpc port",
-			},
-		}...,
-	)
+	f = append(f, authFlags()...)
+	f = append(f, grpcFlags()...)
+	f = append(f, redisFlags()...)
 	f = append(f, commonFlags()...)
 	return append(f, apiflag.NatsFlag()...)
 }
@@ -119,6 +72,66 @@ func commonFlags() []cli.Flag {
 			Name:  "port",
 			Usage: "tcp port at which the server will be available",
 			Value: "9560",
+		},
+	}
+}
+
+func grpcFlags() []cli.Flag {
+	return []cli.Flag{
+		cli.StringFlag{
+			Name:   "user-grpc-host",
+			EnvVar: "USER_API_SERVICE_HOST",
+			Usage:  "user grpc host",
+		},
+		cli.StringFlag{
+			Name:   "user-grpc-port",
+			EnvVar: "USER_API_SERVICE_PORT",
+			Usage:  "user grpc port",
+		},
+		cli.StringFlag{
+			Name:   "identity-grpc-host",
+			EnvVar: "IDENTITY_API_SERVICE_HOST",
+			Usage:  "identity grpc host",
+		},
+		cli.StringFlag{
+			Name:   "identity-grpc-port",
+			EnvVar: "IDENTITY_API_SERVICE_PORT",
+			Usage:  "identity grpc port",
+		},
+	}
+}
+
+func redisFlags() []cli.Flag {
+	return []cli.Flag{
+		cli.StringFlag{
+			Name:   "redis-master-service-host",
+			EnvVar: "REDIS_MASTER_SERVICE_HOST",
+			Usage:  "redis master grpc host",
+		},
+		cli.StringFlag{
+			Name:   "redis-master-service-port",
+			EnvVar: "REDIS_MASTER_SERVICE_PORT",
+			Usage:  "redis master grpc port",
+		},
+	}
+}
+
+func authFlags() []cli.Flag {
+	return []cli.Flag{
+		cli.StringFlag{
+			Name:   "config, c",
+			Usage:  "config file (required)",
+			EnvVar: "OAUTH_CONFIG",
+		},
+		cli.StringFlag{
+			Name:   "pkey, public-key",
+			Usage:  "public key file for verifying jwt",
+			EnvVar: "JWT_PUBLIC_KEY",
+		},
+		cli.StringFlag{
+			Name:   "private-key, prkey",
+			Usage:  "private key file for signing jwt",
+			EnvVar: "JWT_PRIVATE_KEY",
 		},
 	}
 }
