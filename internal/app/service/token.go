ba394af62896de90f375eb4ba8fe4b26daf74aea
wildlifehexagon <crossthebreeze@gmail.com>
2020-01-27 10:06:30 -0600

    refactor: put reused token code into new function

--- internal/app/service/token.go
+++ internal/app/service/token.go
@@ -1,13 +1,9 @@
 package service
 
 import (
-	"context"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
-	"github.com/dictyBase/aphgrpc"
-	"github.com/dictyBase/go-genproto/dictybaseapis/auth"
-	"github.com/dictyBase/modware-auth/internal/jwtauth"
 	"github.com/rs/xid"
 )
 
@@ -19,13 +15,6 @@ type RefreshTokenClaims struct {
 	provider string
 	// Standard JWT claims
 	jwt.StandardClaims
-}
-
-type GenerateTokens struct {
-	ctx      context.Context
-	identity string
-	provider string
-	j        jwtauth.JWTAuth
 }
 
 func generateStandardClaims(expirationMinutes time.Duration) jwt.StandardClaims {
@@ -46,23 +35,4 @@ func generateRefreshTokenClaims(identity string, provider string) RefreshTokenCl
 		provider,
 		generateStandardClaims(refreshTokenExpirationTimeInMins),
 	}
-}
-
-func generateBothTokens(gt *GenerateTokens) (*auth.Token, error) {
-	tkn := &auth.Token{}
-	// generate new claims
-	jwtClaims := generateStandardClaims(jwtExpirationTimeInMins)
-	refTknClaims := generateRefreshTokenClaims(gt.identity, gt.provider)
-	// generate new JWT and refresh token to send back
-	tknStr, err := gt.j.Encode(jwtClaims)
-	if err != nil {
-		return tkn, aphgrpc.HandleError(gt.ctx, err)
-	}
-	tkn.Token = tknStr
-	refTknStr, err := gt.j.Encode(refTknClaims)
-	if err != nil {
-		return tkn, aphgrpc.HandleError(gt.ctx, err)
-	}
-	tkn.RefreshToken = refTknStr
-	return tkn, nil
 }
